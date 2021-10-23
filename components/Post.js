a0494942b44ea7f5206c1ada6db84fa1e284a4f9
hiii-haaa-page <hashi.hiro.0531@gmail.com>
2021-10-23 21:58:31 +0900

    wow

--- components/Post.js
+++ components/Post.js
@@ -8,6 +8,7 @@ export default function Post({ post }) {
       <Link href={`/posts/${post.id}/`}>
         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
           {post.title}
+          wow
         </span>
       </Link>
     </div>
